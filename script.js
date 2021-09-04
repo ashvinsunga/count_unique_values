9f5507d63992bb17f4e2afe9d9c425dd1756e87e
ashvinsunga <ashvinsunga@yahoo.com.ph>
2021-09-04 23:12:13 +0800

    added comment

--- script.js
+++ script.js
@@ -1,31 +1,32 @@
-//1st Solution
+// THE GOAL OF THIS FUNCTION
+// Accepts sorted array and counts the unique values in the array
 
-function countUniqueValue(nums){
-    let collection = {};
-    var count = 0;
-    // If num don't exist on the collection, add 1 to the count and add it to collection
-    for (let num of nums){
-    if (collection[num]){
-        collection[num]++;
-    }else{
-        collection[num] = 1
-        count++;
-        }
-    }
-    return count;
-}
-
-
-//Other solution
-
+// 1st Solution
 // function countUniqueValue(nums){
-//     var i = 0;
-//     for (var j = 1; j < nums.length; j++){
-//         if(nums[i] !== nums[j]){
-//             i++;
-//             nums[i] = nums[j];
+//     let collection = {};
+//     var count = 0;
+//     // If num don't exist on the collection, add 1 to the count and add it to collection
+//     for (let num of nums){
+//     if (collection[num]){
+//         collection[num]++;
+//     }else{
+//         collection[num] = 1
+//         count++;
 //         }
 //     }
-//     return i + 1;
-// }                                 
+//     return count;
+// }
+
+
+//2nd Solution
+function countUniqueValue(nums){
+    var i = 0;
+    for (var j = 1; j < nums.length; j++){
+        if(nums[i] !== nums[j]){
+            i++;
+            nums[i] = nums[j];
+        }
+    }
+    return i + 1;
+}                                 
 console.log(countUniqueValue([1,3,3,4,4,4,7,7,12,12,13]));
